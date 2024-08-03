058ba9223223d1841ad4b5431272f6a602f9149b
unknown <oa.garrido11@gmail.com>
2024-08-03 00:30:49 -0600

    link modifier

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="grid place-items-center">
-      <h1 className="text-3xl">Static Blog Demo</h1>
-      <Link href={"blogs"} className="text-blue-500 text-xl underline">
-        Visita Mi blog
+      <h1 className="m-4 text-3xl">Oscar Garrido</h1>
+      <Link href={"blogs"} className="m-2 text-blue-500 text-xl underline">
+        Visita Mi blog →
       </Link>
-      <Link href={"about"} className="text-blue-500 text-xl underline">
-        + Sobre Mi
+      <Link href={"about"} className="m-2 text-blue-500 text-xl underline">
+        + Sobre Mi →
       </Link>
     </main>
   )
