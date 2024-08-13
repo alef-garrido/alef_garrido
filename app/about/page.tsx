f90ae5e5d87a2d76b5e1711cfc5406e07ffae149
unknown <oa.garrido11@gmail.com>
2024-08-12 19:25:48 -0600

    Layout edit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,14 @@ import Example from "./example.mdx"
 
 export default function AboutPage() {
   return (
-    <main className="mx-auto bg-texture p-6 rounded-lg shadow-lg">
+    <main className="
+      max-w-[420px] 
+      md:max-w-[950px] 
+      md:p-12
+      p-4 
+      bg-texture-xl 
+      rounded-lg 
+      shadow-lg">
       <section className="prose">
         <Example />
       </section>
