f12caa38bd7b368363fba8d8f3b8a248b79d9458
unknown <oa.garrido11@gmail.com>
2024-08-27 13:12:33 -0600

    Content updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <main className="
       max-w-[420px] 
-      md:max-w-[950px] 
+      md:max-w-[800px] 
       md:p-12
       p-4 
       bg-texture-y 
